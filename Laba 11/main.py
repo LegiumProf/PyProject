f5adf2728400923c5597b543a9e8aedb54c3c222
LegiumProf <semyonoffvadick@yandex.ru>
2023-05-18 13:12:08 +0300

    123

--- Laba 11/main.py
+++ Laba 11/main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+def first():
+    class Restaurant():
+        def __init__(self, restaurant_name, cuisine_type):
+            self.restaurant_name = restaurant_name
+            self.cuisine_type = cuisine_type
+        def describe_restaurant(self):
+            print(f"Название рестика - {self.restaurant_name}")
+            print(f"Тип кузины - {self.cuisine_type}")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        def open_restaurant(self):
+            print("Рестик открыт")
 
+    new_restaurant = Restaurant("У Кузины", "вкусная")
+    print(new_restaurant.restaurant_name)
+    print(new_restaurant.cuisine_type)
+    print()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    new_restaurant.describe_restaurant()
+    new_restaurant.open_restaurant()
 
+    print()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    restaurant1 = Restaurant(input("Введите название ресторана"), "фаст фуд")
+    restaurant2 = Restaurant("Хмели Сунели", "Грузинская")
+    restaurant3 = Restaurant("Теремок", "Русская")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    restaurant1.describe_restaurant()
+    restaurant2.describe_restaurant()
+    restaurant3.describe_restaurant()
+
+def third():
+    from random import randint
+    class restaurant():
+        def __init__(self, restaurant_name, cuisine_type):
+            self.restaurant_name = restaurant_name
+            self.cuisine_type = cuisine_type
+            self.rating = randint(1,5)
+        def describe_restaurant(self):
+            print(f"Название рестика - {self.restaurant_name}")
+            print(f"Тип кузины - {self.cuisine_type}")
+            print(f"Рейтинг рестика - {self.rating}")
+
+        def new(self):
+            new_rating = int(input("Введите новый рейтинг ресторана(от 1 до 5): "))
+            while new_rating < 1 or new_rating > 5:
+                print("Ошибка! Вы ввели неверный рейтинг")
+                new_rating = int(input("Введите новый рейтинг ресторана(от 1 до 5): "))
+            self.rating = new_rating
+            print(f"Название рестика - {self.restaurant_name}")
+            print(f"Тип кузины - {self.cuisine_type}")
+            print(f"Рейтинг рестика - {self.rating}")
+    new_restaurant = restaurant("Макдональдс", "Фаст Фуд")
+    new_restaurant.describe_restaurant()
+    new_restaurant.new()
+
+first()
